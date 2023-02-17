ea592173b9dd006d85c38acb9ddb828dfc8ee75f
Maciel <dev.macielsevilha@gmail.com>
2023-02-17 15:07:25 -0300

    creating QueryWrapper

--- postit/app/layout.jsx
+++ postit/app/layout.jsx
@@ -2,6 +2,7 @@ import Nav from './auth/Nav'
 import './globals.css'
 
 import { Roboto } from '@next/font/google'
+import QueryWrapper from './auth/QueryWrapper'
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -16,10 +17,12 @@ export default function RootLayout({ children }) {
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head/>
+      <head />
       <body className={`mx-4 md:mx-48 xl:mx-96 ${roboto.varieble} bg-gray-200`}>
-        <Nav />
-        {children}
+        <QueryWrapper>
+          <Nav />
+          {children}
+        </QueryWrapper>
       </body>
     </html>
   )
