6704c383256930cefda86a2059148ca2b51201bd
Maciel <dev.macielsevilha@gmail.com>
2023-03-01 19:30:20 -0300

    carregando api

--- next13-experiment/app/about/page.jsx
+++ next13-experiment/app/about/page.jsx
@@ -1,10 +1,21 @@
+import Movie from "./Movie"
+
 export default async function About() {
     const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
    const res = await data.json()
-   console.log(res)
+
     return(
         <div>
             <h1 className="text-3xl font-bold underline">About page.</h1>
+          {res.results.map((movie) => (
+          <Movie
+           key={movie.id}
+           id={movie.id}
+           title={movie.title}
+           poster_path={movie.poster_path}
+           release_date={movie.release_date}
+          />
+          ))}
         </div>
     )
-}
+} 
