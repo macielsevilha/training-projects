ddadc267079060769746abeeceb4266a8e59c74b
Maciel <dev.macielsevilha@gmail.com>
2023-03-01 11:54:49 -0300

    teste de api

--- next13-experiment/app/about/page.jsx
+++ next13-experiment/app/about/page.jsx
@@ -1,4 +1,7 @@
-export default function About() {
+export default async function About() {
+    const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
+   const res = await data.json()
+   console.log(res)
     return(
         <div>
             <h1 className="text-3xl font-bold underline">About page.</h1>
